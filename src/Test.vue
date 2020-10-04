b4027259897a50c2a633c888aaa63d2f8a88d235
bobaekang <bkang.dev@gmail.com>
2020-10-04 14:49:31 -0500

    Add another entry point Test.vue

---
+++ src/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Using Test.vue as entry point" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'Test',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
