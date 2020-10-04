f711c0d12e3d5b239551822e988393f9990907bd
bobaekang <bkang.dev@gmail.com>
2020-10-04 14:50:02 -0500

    Implement logic for multi entry point with default

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const BUILD_TARGETS = {
+  app: './App.vue',
+  test: './Test.vue',
+}
+
+const path = BUILD_TARGETS[import.meta.env.VITE_BUILD_TARGET] || './App.vue'
+
+import(`${path}`).then(({ default: App }) => createApp(App).mount('#app'))
